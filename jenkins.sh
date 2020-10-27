e56f3de5fdbab55cb37688addb6b81de48e96a47
Iryna Zaitsava <irinabro85@gmail.com>
2020-10-27 18:31:21 +0300

    test

--- jenkins.sh
+++ jenkins.sh
@@ -1,11 +1,12 @@
 #! /bin/bash
 
+gcloud container clusters get-credentials cluster-1 --zone us-central1-c --project jenkins-2020-292615
+
 #apt-get install jq -y
 #name=`kubectl get pods -A| grep docker | awk -F' ' '{print $2}'`
 #a=`kubectl get pod $name -o json | jq '.status.containerStatuses[] | { "image": .image, "imageID": .imageID }'`
-sudo apt-get install -y gettext-base
-export BUILD_NUMBER=$1
-envsubst < kubectl apply -f docker-deploy.yaml
+
+kubectl apply -f docker-deploy.yaml
 #sleep 200
 #if [ $(curl -s -o /dev/null -I -w "%{http_code}"  http://helloworld-izaitsava.k8s.izaitsava.playpit.by/helloworld-ws/) -ne 200 ]; then
  # kubectl rollout undo deployment $name
